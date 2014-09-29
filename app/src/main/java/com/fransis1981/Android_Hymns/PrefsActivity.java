7a4bf6f0c2cc4b97ff3d49d732fc121c58b86e02
Francesco Vitullo <francesco.vitullo@gmail.com>
2014-09-29 14:23:57 +0200

    NEW - Added skeleton code for preference change listener.

--- app/src/main/java/com/fransis1981/Android_Hymns/PrefsActivity.java
+++ app/src/main/java/com/fransis1981/Android_Hymns/PrefsActivity.java
@@ -3,6 +3,7 @@ package com.fransis1981.Android_Hymns;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 /**
  * Created by Fransis on 22/09/2014.
@@ -15,6 +16,14 @@ public class PrefsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mPrefChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                if (MyConstants.DEBUG) Log.i(MyConstants.LogTag_STR, key + " preference changed.");
+            }
+        };
+
         addPreferencesFromResource(R.xml.prefs);
     }
 }
