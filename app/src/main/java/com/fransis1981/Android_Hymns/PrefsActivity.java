bc46dbaf1e7722b3cb5dd99a09c96609c94fe81e
Francesco Vitullo <francesco.vitullo@gmail.com>
2014-09-29 13:01:24 +0200

    Testing git clone.

--- app/src/main/java/com/fransis1981/Android_Hymns/PrefsActivity.java
+++ app/src/main/java/com/fransis1981/Android_Hymns/PrefsActivity.java
@@ -1,5 +1,6 @@
 package com.fransis1981.Android_Hymns;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
@@ -8,6 +9,8 @@ import android.preference.PreferenceActivity;
  */
 public class PrefsActivity extends PreferenceActivity {
     public static final String PREF_LANGUAGE_SELECTED = "pref_language_selected";
+
+    SharedPreferences.OnSharedPreferenceChangeListener mPrefChangeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
